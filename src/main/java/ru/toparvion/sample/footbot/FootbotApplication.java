0de8fff41d6c9e8d38d3fb34b1fc91674e5bed16
Toparvion <toparvion@gmx.com>
2018-06-05 08:04:31 +0700

    Initial commit
    Signed-off-by: Toparvion <toparvion@gmx.com>

---
+++ src/main/java/ru/toparvion/sample/footbot/FootbotApplication.java
@@ -0,0 +1,12 @@
+package ru.toparvion.sample.footbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FootbotApplication.class, args);
+  }
+}
---
+++ src/test/java/ru/toparvion/sample/footbot/FootbotApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.toparvion.sample.footbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootbotApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
