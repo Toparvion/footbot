198164bab022dbbcebadf4853c2e002c37c5bdb3
Toparvion <toparvion@gmx.com>
2018-06-18 14:17:36 +0700

    Added local Livescore API test request

--- src/main/java/ru/toparvion/sample/footbot/controller/LiveEventsController.java
+++ src/main/java/ru/toparvion/sample/footbot/controller/LiveEventsController.java
@@ -1,10 +1,12 @@
 package ru.toparvion.sample.footbot.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import ru.toparvion.sample.footbot.flow.LiveEventsGateway;
 import ru.toparvion.sample.footbot.model.livescore.Event;
 import ru.toparvion.sample.footbot.model.livescore.LivescoreResponse;
@@ -15,6 +17,7 @@ import ru.toparvion.sample.footbot.model.livescore.LivescoreResponse;
  */
 @RestController
 @AllArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class LiveEventsController {
 
   private final LiveEventsGateway gateway;
@@ -22,5 +25,13 @@ public class LiveEventsController {
   @GetMapping("/events/{matchId}")
   public LivescoreResponse<Event> getEvents(@PathVariable String matchId) {
     return gateway.fetchEvents(matchId);
+  }
+
+  @GetMapping("/dev/test-livescore-api")
+  public Object testLivescoreApi() {
+    log.info("Sending test request to Livescore API.");
+    RestTemplate restTemplate = new RestTemplate();
+    String uri = "http://livescore-api.com/api-client/users/pair.json?key=xj0WfwwQ48WqgMPD&secret=nQK8I0mav0MkKvrhOre4YKV3TRtaDjN0";
+    return restTemplate.getForObject(uri, Object.class);
   }
 }
