3bfb96c982c42cd675a5fce79764c6d16df1e121
Toparvion <toparvion@gmx.com>
2018-06-22 16:07:31 +0700

    Added test method for debugging of integration with Telegram Bot API

--- src/main/java/ru/toparvion/sample/footbot/controller/IndexController.java
+++ src/main/java/ru/toparvion/sample/footbot/controller/IndexController.java
@@ -4,8 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 import ru.toparvion.sample.footbot.model.config.Schedule;
+import ru.toparvion.sample.footbot.telegram.FootBot;
 
 import java.time.ZonedDateTime;
 
@@ -19,6 +24,7 @@ import java.time.ZonedDateTime;
 public class IndexController {
 
   private final Schedule schedule;
+  private final FootBot footBot;
 
   @GetMapping("/")
   public String index() {
@@ -30,6 +36,12 @@ public class IndexController {
   @GetMapping("/schedule")
   public Schedule schedule() {
     return schedule;
+  }
+
+  @GetMapping("/test-bot")
+  public Message testBot(@RequestParam String ping) throws TelegramApiException {
+    log.debug("Получен запрос на отправку сообщения через бота: {}", ping);
+    return footBot.execute(new SendMessage((long) footBot.creatorId(), ping));
   }
 
 }
