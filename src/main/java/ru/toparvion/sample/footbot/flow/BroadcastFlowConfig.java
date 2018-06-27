51ae94df897bb42f5a3e58d83b898778d1afe8e7
Toparvion <toparvion@gmx.com>
2018-06-27 07:32:50 +0700

    Added splitting and filtering of empty events
    Signed-off-by: Toparvion <toparvion@gmx.com>

--- src/main/java/ru/toparvion/sample/footbot/flow/BroadcastFlowConfig.java
+++ src/main/java/ru/toparvion/sample/footbot/flow/BroadcastFlowConfig.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
+import ru.toparvion.sample.footbot.model.sportexpress.event.Event;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.springframework.integration.dsl.IntegrationFlows.from;
 import static org.springframework.integration.dsl.Pollers.fixedDelay;
+import static org.springframework.util.StringUtils.hasText;
+import static ru.toparvion.sample.footbot.model.sportexpress.event.Type.text;
 
 /**
  * Основная конфигурация интеграционного конвейера
@@ -24,10 +27,12 @@ public class BroadcastFlowConfig {
    */
   @Bean
   public IntegrationFlow broadcastFlow(MatchEventsProvider matchEventsProvider) {
-    return IntegrationFlows.from(matchEventsProvider,
-                                 spec -> spec.poller(fixedDelay(15, SECONDS, 0)))
-        .log()
-        .get();
+    return from(matchEventsProvider, spec -> spec.poller(fixedDelay(15, SECONDS, 0)))
+          .split()
+          .filter(Event.class, event -> !(event.getType() == text && !hasText(event.getText())))
+          .<Event>log(message -> String.format("Получено событие матча с типом %s, временем %s и комментарием: %s",
+              message.getPayload().getType(), message.getPayload().getFullMinute(), message.getPayload().getText()))
+          .get();
   }
 
 }
